613467fa12e2d39c671e9001516f539120ddb471
yuliangbing <913932453@qq.com>
2018-12-10 09:05:20 +0800

    Update MD5Util.java

--- jskh-web/src/main/java/com/zptc/gx/util/MD5Util.java
+++ jskh-web/src/main/java/com/zptc/gx/util/MD5Util.java
@@ -3,8 +3,12 @@ package com.zptc.gx.util;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.codec.binary.Base64;
+import org.apache.log4j.Logger;
+
+import com.zptc.gx.controller.util.PageController;
 
 public class MD5Util {
+	private static Logger logger = Logger.getLogger(MD5Util.class);
 	
 	Base64 base64 = new Base64();
 
