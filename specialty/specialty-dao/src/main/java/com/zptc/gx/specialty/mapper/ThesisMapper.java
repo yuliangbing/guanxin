ad4b26c986c930644cf4bbd5190a14c18badcb04
WengJinYue <1584953025@qq.com>
2019-04-25 18:27:33 +0800

    1.修改论文的编辑、添加、查看功能
    2.增加论文接口

--- specialty/specialty-dao/src/main/java/com/zptc/gx/specialty/mapper/ThesisMapper.java
+++ specialty/specialty-dao/src/main/java/com/zptc/gx/specialty/mapper/ThesisMapper.java
@@ -1,5 +1,8 @@
 package com.zptc.gx.specialty.mapper;
 
+import java.util.List;
+import java.util.Map;
+
 import com.zptc.gx.specialty.entity.Thesis;
 
 public interface ThesisMapper {
@@ -14,4 +17,12 @@ public interface ThesisMapper {
     int updateByPrimaryKeySelective(Thesis record);
 
     int updateByPrimaryKey(Thesis record);
+    
+  //获取全部列表数据
+   	List<Thesis> getThesisList(Object data);
+   	
+   	//统计数据条数
+   	int selectCounts(Map<String,Object> counts);
+   	
+   	int updateByPrimaryKeyDel(Thesis thesis);
 }
