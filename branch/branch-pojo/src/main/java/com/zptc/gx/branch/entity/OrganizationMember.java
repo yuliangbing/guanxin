925949a55079bf76abad7eabde137922ec9148be
L6617 <1014514261@qq.com>
2019-05-08 21:36:08 +0800

    添加删除的回滚

--- branch/branch-pojo/src/main/java/com/zptc/gx/branch/entity/OrganizationMember.java
+++ branch/branch-pojo/src/main/java/com/zptc/gx/branch/entity/OrganizationMember.java
@@ -83,5 +83,12 @@ public class OrganizationMember {
         this.modifyUser = modifyUser == null ? null : modifyUser.trim();
     }
 
+	@Override
+	public String toString() {
+		return "OrganizationMember [id=" + id + ", position=" + position + ", name=" + name + ", status=" + status
+				+ ", createTime=" + createTime + ", createUser=" + createUser + ", modifyTime=" + modifyTime
+				+ ", modifyUser=" + modifyUser + "]";
+	}
+
 	
 }
