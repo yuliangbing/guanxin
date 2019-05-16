c57a665f95da4719049b013c60e7f0c1f1526d28
hsdwt <hsdwt2749@163.com>
2019-05-16 10:39:46 +0800

    校企合作

---
+++ jskh-web/src/main/webapp/WEB-INF/jsp/school_enterprise_cooperation/schEntCoo_check.jsp
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ include file="/WEB-INF/Taglib.jsp"%>
+<%@ include file="/WEB-INF/Common.jsp"%>
+<html>
+<head>
+<link rel="stylesheet" href="/static/public/layui/css/layui.css">
+<link rel="stylesheet" href="/static/public/css/xadmin.css">
+		<script type="text/javascript" src="${path}/static/js/school_enterprise_cooperation/schEntCoo_List.js"></script>
+<script src="${path}/static/public/layui/layui.js" type="text/javascript"></script>
+<title>浙江邮电职业技术学院管理系统</title>
+</head>
+<body>
+		<form class="layui-form" onsubmit="return false;">
+
+			<div class="layui-form-item" style="margin-left:10%;margin-top:2%">
+				<div class="layui-inline">
+					<label class="layui-form-label">主键</label>
+					<div class="layui-input-inline">
+						<input name="id" id="id" disabled  autocomplete="off" class="layui-input">
+					</div>
+				</div>
+				<div class="layui-inline">
+					<label class="layui-form-label">时间</label>
+					<div class="layui-input-inline">
+						<input name="date" id="date" disabled autocomplete="off" class="layui-input">
+					</div>
+				</div>
+				
+				<div class="layui-inline">
+					<label class="layui-form-label">合作内容</label>
+					<div class="layui-input-inline">
+						<input name="content" id="content" disabled autocomplete="off" class="layui-input">
+					</div>
+				</div>
+			
+				<div class="layui-inline">
+					<label class="layui-form-label">合作单位</label>
+					<div class="layui-input-inline">
+						<input name="units" id="units" disabled autocomplete="off" class="layui-input">
+					</div>
+				</div>
+				<div class="layui-inline">
+					<label class="layui-form-label">参与单位</label>
+					<div class="layui-input-inline">
+						<input name="participants" id="participants" disabled autocomplete="off" class="layui-input">
+					</div>
+				</div>
+				<div class="layui-inline">
+					<label class="layui-form-label">取得成果</label>
+					<div class="layui-input-inline"  >
+						<input name="achievements" id="achievements" disabled autocomplete="off" class="layui-input">
+					</div>
+				</div>
+				<div class="layui-inline">
+					<label class="layui-form-label">专业名称</label>
+					<div class="layui-input-inline">
+						<input name="specialtyName"  id="specialtyName" disabled autocomplete="off" class="layui-input">
+					</div>
+				</div>
+				
+			</div>
+		</form>
+	</body>
+	<script src="${path}/static/public/jquery/jquery-3.3.1.min.js" type="text/javascript" charset="utf-8"></script>
+   	<script src="${path}/static/public/lib/layui/layui.js" type="text/javascript" charset="utf-8"></script>
+   	<script>
+   	function init(data) {
+
+   		$("#id").val(data.id);
+		$("#date").val((data.date.split(' '))[0]);
+		$("#content").val(data.content);
+		$("#units").val(data.units);
+		$("#participants").val(data.participants);
+		$("#achievements").val(data.achievements);
+		$("#specialtyName").val(data.specialtyName);
+	}
+   	</script>
+</html>
---
+++ jskh-web/src/main/webapp/WEB-INF/static/js/school_enterprise_cooperation/schEntCoo_List.js
@@ -0,0 +1,226 @@
+//function ajax_h(form,url,object,ids){
+//			//获取下拉列表(公共方法)
+//			$.ajax({
+//				url:url,
+//				type:"POST",
+//				dataType:"json",
+//				success:function(data){
+//					console.log(data);
+//					//layer.msg("获取成功");
+//					console.log(data.data.length);
+//					if (data.code == 0) {
+//							let option = "";
+//							for (let i=0;i<data.data.length;i++) {
+//								option += "<option value='"+data.data[i].id+"'>"+data.data[i].name+"</option>";
+//							}
+//							$("#"+object).append(option);
+//							form.render('select');
+//						
+//					} else {
+//						layer.msg("请检查网络连接！");
+//					}
+//					
+//				} ,error:function(code){
+//		           layer.alert("发生错误,请联系管理员");
+//		        }
+//			});
+//		}
+layui.use('element', function() {
+				var element = layui.element;
+
+			});
+layui.use(['form', 'table', 'laydate'], function() {
+				var form = layui.form;
+				var table = layui.table;
+				
+				/*
+				 实现时间选择
+				 */
+				var laydate = layui.laydate;
+
+				//执行一个laydate实例
+				laydate.render({
+					elem: '#date' //指定元素	
+					,range: '~' //或 range: '~' 来自定义分割字符
+				});
+	//加载数据表格
+	  var tableIns = table.render({
+		    elem: '#test'
+		    ,url:window.path +'/SchoolEnterpriseCooperation/getSchoolEnterpriseCooperation'
+		    ,title: '用户数据表'
+		    ,toolbar:'#toolbarDemo'
+		    ,page: true
+		    ,cols: [[
+		      {type: 'checkbox', fixed: 'left'}
+		      ,{field:'id', title:'主键', width:100,sort: true,align:'center'}
+		      ,{field:'date', title:'时间', width:200,align:'center'}	     
+		      ,{field:'content', title:'合作内容', width:150,align:'center'}
+		      ,{field:'units', title:'合作单位', width:150,align:'center'} 
+		      ,{field:'participants', title:'参与人员', width:130,align:'center'}
+		      ,{field:'achievements', title:'取得成果', width:130,align:'center'}
+		      ,{field:'specialtyName', title:'专业名称', width:130,align:'center'}
+		      ,{field:'specialtyId', title:'专业id', width:130,hide:true }
+		     // ,{field:'status', title:'状态(1=正常，2=删除)', width:180,hide:true}
+		     // ,{field:'create_time', title:'创建时间', width:150,  hide:true}
+		   //   ,{field:'create_user', title:'创建人', width:150, hide:true }
+		     // ,{field:'modify_time', title:'修改时间', width:150,  hide:true}
+		     // ,{field:'modify_user', title:'修改人', width:130, hide:true }
+		      ,{fixed: 'right', title:'操作', toolbar: '#barDemo', width:237,align:'center'}
+		    ]]
+		  });  
+	  //监听列工具事件
+	  table.on('tool(test)', function(obj){
+	    var data = obj.data;
+	    
+	    var layEvent = obj.event;
+	    if(layEvent === 'detail'){//查看
+	    	
+	    	layer.open({
+				title:"查看",
+	    		type:2,
+	    		content:['/toPage?page=school_enterprise_cooperation/schEntCoo_check'],
+	    		maxmin:true,
+	    		resize:false,
+	    		area:['90%','90%'],
+	    		success : function(layero, index) {
+					// 获取子页面的iframe
+					var iframe = window['layui-layer-iframe' + index];
+					// 向子页面的全局函数child传参
+					iframe.init(data);
+				} 
+	    	});
+	    }else if(layEvent === 'del'){//删除
+	    	layer.confirm('真的删除行么', function(index) {
+				/*obj.del(); //删除对应行（tr）的DOM结构，并更新缓存
+				layer.close(index);
+				//向服务端发送删除指令*/	
+	    		console.log(data.id);
+				$.ajax({
+					url:'/SchoolEnterpriseCooperation/delSchoolEnterpriseCooperation',
+					type:"POST",
+					data:{id:data.id},
+					dataType:"json",
+					success:function(data){
+						var nowPage = tableIns.config.page.curr;//返回当前页数
+			        	var reloadPage = (nowPage-1) > 0? nowPage:1;
+			        	//console.log((nowPage-1));
+			        	//console.log(reloadPage);
+						layer.msg("删除成功");
+						layer.close(index);
+		    			tableIns.reload({
+		    				page:{
+		    					curr:reloadPage
+		    				}
+		    			});
+					}
+				});
+				});
+	    }else if(layEvent === 'edit'){//编辑
+	    	layer.open({
+	    		title:"编辑",
+	    		type:2,
+	    		content:['/toPage?page=school_enterprise_cooperation/schEntCoo_update'],
+	    		maxmin:true,
+	    		resize:false,
+	    		area:['90%','90%'],
+	    		success : function(layero, index) {
+					// 获取子页面的iframe
+					var iframe = window['layui-layer-iframe' + index];
+					// 向子页面的全局函数child传参
+					iframe.init(data);
+				} 
+			});
+			}
+		});
+
+		/* 搜索功能 */
+	  form.on('submit(search)', function(data) {
+			/*layer.alert(JSON.stringify(data.field));*/
+			let arr = {};
+			arr = data.field;
+			if(arr.data != "" && arr.date != null){
+				arr.date1 = data.field.date.split('~')[0].replace(/(^\s*)|(\s*$)/g, "");
+				arr.date2 = data.field.date.split('~')[1];
+			}
+			tableIns.reload({
+				where:arr,
+				page: {
+					curr: 1
+				}
+			});
+			return false; //阻止表单跳转。如果需要表单跳转，去掉这段即可。
+		});
+	  
+		
+		layui.use('element', function() {
+			var element = layui.element;
+
+		});
+
+		layui.use(['form','laydate'], function() {
+			var form = layui.form;
+			
+		
+			/*
+			 实现文件时间选择
+			 */
+			var laydate = layui.laydate;
+			
+			laydate.render({
+				elem: '#date' //指定元素
+			});
+			
+	//添加按钮点击事件
+	  $("#insert").click(function(){
+	  	layer.open({
+	  		title:"添加",
+	  		type:2,
+	  		content:['/toPage?page=school_enterprise_cooperation/schEntCoo_insert'],
+	  		maxmin:true,
+	  		resize:false,
+	  		area:['90%','90%']
+	  	});
+	  });
+	  //批量删除
+	  table.on('toolbar(test)', function(obj){
+		    var checkStatus = table.checkStatus(obj.config.id);
+		    //console.log(JSON.stringify(checkStatus.data.id));
+		    
+		    switch(obj.event){
+		      case 'delData':
+		        var data = checkStatus.data;
+		        var param = [{}];
+		       // layer.alert(JSON.stringify(data));
+		        for(var i=0;i< data.length;i++){
+		        	param = data[i].id;
+//		        	layer.alert(JSON.stringify(data[i].id));
+		        	console.log(param);
+		        	//向服务端发送删除指令*/		
+					$.ajax({
+						url:'/SchoolEnterpriseCooperation/delSchoolEnterpriseCooperation',
+						type:"POST",
+						data:{id:param},
+						dataType:"json",
+						success:function(data){
+							var nowPage = tableIns.config.page.curr;//返回当前页数
+				        	var reloadPage = (nowPage-1) > 0? nowPage:1;
+							layer.msg("删除成功");
+							//layer.close(index);
+			    			tableIns.reload({
+			    				page:{
+			    					curr:reloadPage
+			    				}
+			    			});
+						}
+					});
+		        	
+		        	
+		        }
+		       //layer.alert(JSON.stringify(param));
+		        
+		      break;
+		    };
+		    
+		  });
+});
+});
