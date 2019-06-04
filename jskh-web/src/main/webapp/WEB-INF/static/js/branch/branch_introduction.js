f8f7dd4d0c61be6bb8cb3253dc2eb7e97fe9951d
L6617 <1014514261@qq.com>
2019-06-04 16:17:24 +0800

    分院js修改

--- jskh-web/src/main/webapp/WEB-INF/static/js/branch/branch_introduction.js
+++ jskh-web/src/main/webapp/WEB-INF/static/js/branch/branch_introduction.js
@@ -1,11 +1,27 @@
+var datas;
+$.ajax({
+		type : "POST",
+		url : '/branch/getBranchText',
+		// 预期服务器返回数据的类型
+		dataType : "json",
+		success : function(data) {
+			datas = data;
+			
+		},
+		error : function(code) {
+			alert("发生错误,请联系管理员");
+		}
+		
+	});
 layui.use([ 'table', 'form', 'laydate' ], function() {
 	var table = layui.table;
 	var form = layui.form;
 	var laydate = layui.laydate;
-
 	laydate.render({
 		elem : '#date' // 指定元素
 	});
+	
+	initBranch();
 })
 
 function save() {
@@ -27,13 +43,12 @@ function save() {
 		dataType : "json",
 		success : function(data) {
 			if (data) {
-				//console.log($.param(params));
+				console.log($.param(params));
 				if (data.code == 0) {
 					layer.msg("成功");
-					setTimeout(function() {
-						parent.window.location.reload();
-					}, 500);
-				} else if (data.code != 0) {
+					 location.reload() ;
+				}
+				 else if (data.code != 0) {
 					layer.msg(data.msg);
 				}
 			}
@@ -47,4 +62,17 @@ function save() {
 	});
 			
 	return false;
+}
+
+function initBranch(){
+	//console.log(JSON.stringify(data));
+	var obj2 = eval(datas);
+	//console.log();
+	$("#date").val(obj2.data[0].date.split(' ')[0]);
+	var specialty_info = "";
+	var branch_characteristic="";
+	specialty_info+=obj2.data[0].specialty_info;
+	branch_characteristic+=obj2.data[0].branch_characteristic;
+	situationUE.setContent(specialty_info);
+	characteristicUE.setContent(branch_characteristic);
 }
